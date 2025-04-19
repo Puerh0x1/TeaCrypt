4f0088fb55d01b90ab1ab8a8f105489d30b50961
Puerh0x1 <puerh0x1@protonmail.ch>
2025-04-19 07:54:56 +0300

    RSA JWK save added

--- KEYSAVE/jwk_save.py
+++ KEYSAVE/jwk_save.py
@@ -1,2 +1,19 @@
 import base64
 import json
+
+def toBase64url():
+    num_bytes = number.to_bytes((number.bit_length() + 7) // 8, byteorder='big')
+    return base64.urlsafe_b64encode(num_bytes).rstrip(b'=').decode('utf-8')
+
+
+def rsaSave(e, n, d, kid, alg='RS256'):
+    jwk = {
+        'kty': 'RSA',
+        'e': toBase64url(e),
+        'n': toBase64url(n), 
+        'd': toBase64url(d),
+        'alg': alg,
+        'kid': kid # Идентификатор ключа
+    }
+    with open('rsa_key.jwk', 'w') as f:
+        f.write(jwk_json)
--- api.py
+++ api.py
@@ -1,6 +1,10 @@
 from ALGS.RSA.mathKeyGen import *
+from KEYSAVE.jwk_save import *
 
-genRes = mainGeneration(9000)
-
+genRes = mainGeneration(2048)
+n = genRes[2]
+e = genRes[4]
+d = genRes[5]
+rsaSave(e, n, d, "MAIN-KEY")
 
 
